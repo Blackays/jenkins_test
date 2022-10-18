f7d254ac6638c806c5a2bfed993c26d4dab6e33f
Blackays <mni01051999@gmail.com>
2022-10-18 10:39:52 +0200

    test

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -22,4 +22,4 @@ usermod -aG docker ec2-usermod
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-mkdir /home/user
+mkdir -p /home/user
